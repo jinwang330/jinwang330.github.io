7650ec4c0e5221d724534059bef6eea23280d704
jinwang330 <54658761+jinwang330@users.noreply.github.com>
2023-03-21 07:46:12 +0000

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+const messageForm = document.getElementById('message-form');
+const messageInput = document.getElementById('message-input');
+const chatBox = document.getElementById('chat-box');
+
+messageForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const message = messageInput.value.trim();
+  if (message) {
+    const newMessage = document.createElement('div');
+    newMessage.innerText = message;
+    chatBox.appendChild(newMessage);
+    messageInput.value = '';
+    chatBox.scrollTop = chatBox.scrollHeight;
+  }
+});
