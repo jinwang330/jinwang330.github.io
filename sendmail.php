4a126adad924afb71c2301c09e32df6f26098df7
jinwang330 <54658761+jinwang330@users.noreply.github.com>
2023-03-21 14:50:42 +0000

    Update sendmail.php

--- sendmail.php
+++ sendmail.php
@@ -1,22 +1,44 @@
-<?php
+// Load PHPMailer library
+require 'PHPMailer/PHPMailer.php';
+require 'PHPMailer/SMTP.php';
+require 'PHPMailer/Exception.php';
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+// Check if form is submitted
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Retrieve form data
     $name = $_POST['name'];
     $email = $_POST['email'];
     $message = $_POST['message'];
 
-    // Set email parameters
-    $to = 'jinwang330gmail.com';
-    $subject = 'New Contact Form Submission';
-    $headers = "From: $email\r\n" .
-               "Reply-To: $email\r\n" .
-               "X-Mailer: PHP/" . phpversion();
+    // Create a new PHPMailer instance
+    $mail = new PHPMailer(true);
 
-    // Send email
-    if (mail($to, $subject, $message, $headers)) {
+    try {
+        // Set SMTP parameters
+        $mail->SMTPDebug = SMTP::DEBUG_OFF;
+        $mail->isSMTP();
+        $mail->Host = 'smtp.gmail.com';
+        $mail->SMTPAuth = true;
+        $mail->Username = 'yourgmail@gmail.com'; // Replace with your Gmail address
+        $mail->Password = 'yourgmailpassword'; // Replace with your Gmail password or an app password if 2FA is enabled
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+        $mail->Port = 587;
+
+        // Set email parameters
+        $mail->setFrom($email, $name);
+        $mail->addAddress('yourgmail@gmail.com'); // Replace with your Gmail address
+        $mail->Subject = 'New Contact Form Submission';
+        $mail->Body = $message;
+
+        // Send email
+        $mail->send();
+
         echo 'Email sent successfully!';
-    } else {
-        echo 'Error sending email.';
+    } catch (Exception $e) {
+        echo 'Error sending email: ' . $mail->ErrorInfo;
     }
 }
-?>
