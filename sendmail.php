1e83574158bcc2ada10603400399b852e4ccafd3
jinwang330 <54658761+jinwang330@users.noreply.github.com>
2023-03-22 12:14:10 +0000

    Update sendmail.php

--- sendmail.php
+++ sendmail.php
@@ -1,44 +1,37 @@
-// Load PHPMailer library
-require 'PHPMailer/PHPMailer.php';
-require 'PHPMailer/SMTP.php';
-require 'PHPMailer/Exception.php';
-
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
-use PHPMailer\PHPMailer\Exception;
-
-// Check if form is submitted
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Retrieve form data
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $message = $_POST['message'];
-
-    // Create a new PHPMailer instance
-    $mail = new PHPMailer(true);
-
-    try {
-        // Set SMTP parameters
-        $mail->SMTPDebug = SMTP::DEBUG_OFF;
-        $mail->isSMTP();
-        $mail->Host = 'smtp.gmail.com';
-        $mail->SMTPAuth = true;
-        $mail->Username = 'jinwang330@gmail.com'; // Replace with your Gmail address
-        $mail->Password = 'xloiyolkaysrjfsv'; // Replace with your Gmail password or an app password if 2FA is enabled
-        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-        $mail->Port = 587;
-
-        // Set email parameters
-        $mail->setFrom($email, $name);
-        $mail->addAddress('jinwang330@gmail.com'); // Replace with your Gmail address
-        $mail->Subject = 'New Contact Form Submission';
-        $mail->Body = $message;
-
-        // Send email
-        $mail->send();
-
-        echo 'Email sent successfully!';
-    } catch (Exception $e) {
-        echo 'Error sending email: ' . $mail->ErrorInfo;
+<?php
+if($_POST) {
+    $name = "";
+    $email = "";
+    $message = "";
+ 
+    if(isset($_POST['name'])) {
+        $name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
     }
+ 
+    if(isset($_POST['email'])) {
+        $email = str_replace(array("\r", "\n", "%0a", "%0d"), '', $_POST['email']);
+        $email = filter_var($email, FILTER_VALIDATE_EMAIL);
+    }
+ 
+    if(isset($_POST['message'])) {
+        $message = htmlspecialchars($_POST['message']);
+    }
+ 
+    $headers = 'From: ' . $name . ' <' . $email . '>' . "\r\n" . 'Reply-To: ' . $email . "\r\n";
+    $subject = 'New message from your website';
+    $body = 'Name: ' . $name . "\n\n" . 'Email: ' . $email . "\n\n" . 'Message: ' . $message;
+ 
+    $send_email_to = 'jinwang330@gmail.com';
+ 
+    if(mail($send_email_to, $subject, $body, $headers)) {
+        $result = array('message' => 'Thank you for contacting me. I will get back to you shortly.', 'status' => 'success');
+    } else {
+        $result = array('message' => 'Sorry, an error occurred. Please try again later.', 'status' => 'error');
+    }
+ 
+    echo json_encode($result);
+} else {
+    header('Location: index.html');
 }
+?>
+
